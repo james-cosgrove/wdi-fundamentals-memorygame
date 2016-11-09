7bdb4fc3c075e511608c8cdca58b62a1fde1b7ef
james-cosgrove <cosgrove.jamesc@gmail.com>
2016-11-09 20:16:12 +1100

    added logic to came and modified style

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,11 @@
 
 var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
+var cardTwo = "king";
+var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	result = window.alert("You found a match!");
+}  else {
+	result = window.alert("Sorry, try again.")
+}
