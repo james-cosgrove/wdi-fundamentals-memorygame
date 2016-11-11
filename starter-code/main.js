00e26fa7ba482d20469bbdbbd23a1cc127730e8a
james-cosgrove <cosgrove.jamesc@gmail.com>
2016-11-11 11:56:20 +1100

    made changes to game login and html

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,24 @@
 
+//var cards ['queen', 'queen', 'king', 'king']
+
+
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
-
+/*
 if (cardTwo === cardFour) {
 	result = window.alert("You found a match!");
 }  else {
 	result = window.alert("Sorry, try again.")
+}*/
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    board.appendChild(cardElement);
+  }
 }
